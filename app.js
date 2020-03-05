b28a32914d200a54e96560f65150644c0c3f62d4
Abdullahi Hassan <ahas1103@gmail.com>
2020-03-04 20:52:45 -0600

    Port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteJSON = require('./db/db.json');
-const PORT = 9020;
+const PORT = process.env.PORT ||9020;
 
 const app = express();
 

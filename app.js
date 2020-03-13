6bd07ee21343487e8c1cfcea4adc3a61c2c960a6
Abdullahi Hassan <ahas1103@gmail.com>
2020-03-12 23:22:31 -0500

    final

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const noteJSON = require('./db/db.json');
-const PORT = process.env.PORT ||9020;
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('./'));
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-  res.json(noteJSON);
-});
-
-app.post('/api/notes', (req, res) => {
-const lastId = noteJSON.length ? Math.max(...(noteJSON.map(note => note.id))) : 0;
-const id = lastId + 1;
-noteJSON.push( { id, ...req.body} );
-res.json(noteJSON.slice(-1));
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-let note = noteJSON.find( ({ id }) => id === JSON.parse(req.params.id));
-noteJSON.splice( noteJSON.indexOf(note), 1);
-res.end("Note deleted");
-});
-
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
